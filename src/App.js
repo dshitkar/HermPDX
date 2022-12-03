49d9b550b0fed45f228861b9f77c64a103778bb3
AnvithaPasumarthi <91653071+AnvithaPasumarthi@users.noreply.github.com>
2022-12-02 18:31:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,4 +20,117 @@ const App = () => {
     }
   }
 
-  
+ // This function is used to draw the root.
+  const drawRoute = (geoJson, map) => {
+    if (map.getLayer('route')) {
+      map.removeLayer('route')
+      map.removeSource('route')
+    }
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geoJson
+      },
+      paint: {
+        'line-color': '#4a90e2',
+        'line-width': 6
+
+      }
+    })
+  }
+  // Delivery markers
+  const addDeliveryMarker = (lngLat, map) => {
+    const element = document.createElement('div')
+    element.className = 'marker-delivery'
+    new tt.Marker({
+      element: element
+    })
+      .setLngLat(lngLat)
+      .addTo(map)
+  }
+
+  // Used to set the map
+  useEffect(() => {
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    }
+    const destinations = []
+
+    // Key is given here
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+
+      },
+      center: [longitude, latitude],
+      zoom: 14,
+    })
+    setMap(map)
+
+    // Setting the delivery driver marker
+    const addMarker = () => {
+      const popupOffset = {
+        bottom: [0, -25]
+      }
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML('You are here!')
+      const element = document.createElement('div')
+      element.className = 'marker'
+
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(map)
+
+      //Updating the location when the marker is dragged.
+      marker.on('dragend', () => {
+        const lngLat = marker.getLngLat()
+        setLongitude(lngLat.lng)
+        setLatitude(lngLat.lat)
+      })
+
+      marker.setPopup(popup).togglePopup()
+
+    }
+    addMarker()
+
+    // Sorting the destinations.
+    // This function will be called everytime a new marker is added
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+      const callParameters = {
+        key: process.env.REACT_APP_TOM_TOM_API_KEY,
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      }
+
+      return new Promise((resolve, reject) => {
+        ttapi.services
+          .matrixRouting(callParameters)
+          .then((matrixAPIResults) => {
+            const results = matrixAPIResults.matrix[0]
+            const resultsArray = results.map((result, index) => {
+              return {
+                location: locations[index],
+                drivingtime: result.response.routeSummary.travelTimeInSeconds,
+              }
+            })
+            resultsArray.sort((a, b) => {
+              return a.drivingtime - b.drivingtime
+            })
+            const sortedLocations = resultsArray.map((result) => {
+              return result.location
+            })
+            resolve(sortedLocations)
+          })
+      })
+    } 
